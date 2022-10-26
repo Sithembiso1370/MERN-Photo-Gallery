0ea609511476920eded717b7e084098de14fde2d
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-10-26 17:25:50 +0200

    Frontend and backend running on one port

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
 import AppRouter from './router/AppRouter';
+import store from './store/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AppRouter/>
-  </React.StrictMode>
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
 );
 
